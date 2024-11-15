c643254435fb8f4abfd97f82ccb3c29c1eccd0b6
spkrrr <admin@spkrrr.com>
2024-11-15 17:57:48 +0800

    init

--- site.config.ts
+++ site.config.ts
@@ -9,7 +9,7 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: 'DEMO',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
   author: 'Travis Fischer',
 
